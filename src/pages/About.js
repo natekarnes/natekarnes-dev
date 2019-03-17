e92eaa0666e994f40992d1af4197021a7ed64809
Nate Karnes <natekarnes@icloud.com>
2019-03-17 11:36:40 -0700

    Added .scss files

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Header from '../components/Header.js';
+import '../stylesheets/Home.scss';
+import '../stylesheets/index.scss';
 
 const About = () => (
     <div>
