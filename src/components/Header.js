3cc94006f3ca4955faac9f8097b5c40f3a9529b8
Nate Karnes <natekarnes@icloud.com>
2019-04-03 10:27:11 -0700

    Navbar update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+// import Nav from 'react-bootstrap/Nav';
 import '../stylesheets/App.scss';
 import '../stylesheets/Header.scss';
+import logoLight from '../images/logoLight.png'
+// import logoDark from '../images/logoDark.png'
 
 
 const Header = () => (
-    <div>
-<Navbar bg="dark" variant='dark'>
-    <Nav className="justify-content-center">
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link disabled href="/about">About</Nav.Link>
-        <Nav.Link disabled href="/skills">Skills</Nav.Link>
-        <Nav.Link disabled href="/contact">Contact</Nav.Link>
-    </Nav>
-</Navbar>
-    </div>
+    <Navbar bg="dark">
+    <Navbar.Brand href="/">
+      <img
+        src={logoLight}
+        width="40"
+        height="40"
+        className="d-inline-block align-top"
+        alt="logoLight"
+      />
+    </Navbar.Brand>
+  </Navbar>
       )
       export default Header;
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,9 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
-// import Card from 'react-bootstrap/Card';
 import mobileEmoji from '../images/mobileEmoji.png'
 import macbookEmoji from '../images/macbookEmoji.png'
 import developerEmoji from '../images/developerEmoji.png'
-// import responsiveWebDevelopment from '../images/responsiveWebDevelopment.jpg'
-// import webDesign from '../images/webDesign.jpg'
-// import contentManagement from '../images/contentManagement.jpg'
-// import technicalSupport from '../images/technicalSupport.jpg'
 import '../stylesheets/Home.scss';
 import '../stylesheets/App.scss';
 import Header from '../components/Header.js';
@@ -29,29 +24,15 @@ const Home = () => (
 {/* Emojis */}
     <Row>
         <Col>
-            <Image id="developerEmoji" width="50" src={developerEmoji} fluid />
+            <Image id="developerEmoji" width="50" src={developerEmoji} fluid />{' '}
             <Image id="macbookEmoji" width="50" src={macbookEmoji} fluid />
             <Image id="mobileEmoji" width="50" src={mobileEmoji} fluid />
         </Col>
     </Row>
     </div>
-{/* First Row of Boxs */}
-    {/* <div id="firstRow">
-    <Row>
-    <Col xs={6} md={3}>
-    <Card.Img id="responsiveWebDevelopment" width="100" src={responsiveWebDevelopment}/>
-    </Col>
-    <Col xs={6} md={3}>
-    <Card.Img id="webDesign" width="100" src={webDesign}/>
-    </Col>
-    <Col xs={6} md={3}>
-    <Card.Img id="contentManagement" width="100" src={contentManagement}/>
-    </Col>
-    <Col xs={6} md={3}>
-    <Card.Img id="technicalSupport" width="100" src={technicalSupport}/>
-    </Col>
-  </Row>
-    </div> */}
+{/* Contact Form */}
+
+
     
     <footer id="footer">
         <Footer/>
